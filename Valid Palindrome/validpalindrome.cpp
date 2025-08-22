56f05f2793085fe6bc8c23332dbfb073e1195a85
Abdullah Ali <abdullahpasari123@gmail.com>
2025-08-23 00:26:33 +0500

    Solved Valid Palindrome Problem

---
+++ Valid Palindrome/validpalindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.length() - 1;
+        while(start <= end)
+        {
+            if(!isalnum(s[start]))
+            {
+                start++;
+                continue;
+            }
+            if(!isalnum(s[end]))
+            {
+                end--;
+                continue;
+            }
+            if(tolower(s[start]) != tolower(s[end]))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
