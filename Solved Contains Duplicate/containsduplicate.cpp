9cdbb41c434baaaf97696fe941065fd10d8f8217
Abdullah Ali <abdullahpasari123@gmail.com>
2025-08-28 01:54:11 +0500

    Solved Contains Duplicate Problem

---
+++ Solved Contains Duplicate/containsduplicate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> temp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(temp.find(nums[i]) != temp.end())
+                return true;
+            temp.insert(nums[i]);
+        }
+        return false;
+    }
+};
