857d706f34e915de6238bb0fe0a621998492baff
Abdullah Ali <abdullahpasari123@gmail.com>
2025-11-03 18:47:45 +0500

    Solved pow(x,n) problem

---
+++ Pow (x, n)/pow.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        bool check = false;
+        if(N < 0){
+            N *=-1;
+            check = true;
+        }
+        double res = 1;
+        while(N > 0)
+        {
+            if(N%2 == 1)
+                res*=x;
+            x*=x;
+            N/=2;
+        }
+        if(check)
+            return 1/res;
+        return res;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cout << "1 raised to the power of -2147483648: " << s.myPow(1, -2147483648) << endl;
+}
